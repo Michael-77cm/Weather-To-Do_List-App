da895ffda503661c36cb8bbcce7f3d0f79b99266
Michael-77cm <michaelbello175@gmail.com>
2026-03-11 02:19:26 +0000

    updated correct admin.py file in todo folder

--- todo_weather_project/todo/admin.py
+++ todo_weather_project/todo/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import TodoItem
 
-# Register your models here.
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'priority', 'category', 'completed', 'created_date', 'due_date')
+    list_filter = ('completed', 'priority', 'category', 'created_date')
+    search_fields = ('title', 'description')
+    date_hierarchy = 'created_date'
+    ordering = ('-created_date',)
+    list_editable = ('completed', 'priority')
+    
+    fieldsets = (
+        ('Task Information', {
+            'fields': ('title', 'description', 'user')
+        }),
+        ('Status', {
+            'fields': ('completed', 'priority', 'category')
+        }),
+        ('Dates', {
+            'fields': ('due_date',)
+        }),
+    )
