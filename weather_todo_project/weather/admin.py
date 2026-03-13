89cabe8e1c8f211b9689c45309aa1c26ab570fad
Michael-77cm <michaelbello175@gmail.com>
2026-03-13 12:58:14 +0000

    updated admin.py file in weather folder

--- weather_todo_project/weather/admin.py
+++ weather_todo_project/weather/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WeatherSearch
 
-# Register your models here.
+@admin.register(WeatherSearch)
+class WeatherSearchAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'country', 'temperature', 'searched_at')
+    list_filter = ('user', 'country', 'searched_at')
+    search_fields = ('city', 'country', 'user__username')
+    date_hierarchy = 'searched_at'
