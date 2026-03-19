a4dacf31c100627ee69cb055280f02f337652f53
Michael-77cm <michaelbello175@gmail.com>
2026-03-19 15:26:23 +0000

    updated settings.py & env.py with SK

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ load_local_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+1+^(5o&s=@_d%s)_oe70$&v#*^c%3*b+@gv^qr+kx@cp&-4+('
+SECRET_KEY = os.environ.get("SECRET_KEY") 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
